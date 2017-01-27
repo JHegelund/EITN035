cda381d7466f1d128696f766801fa0b19bc04c7d
gustavryrlen <gustavryrlen@gustavs-MacBook-Pro-2.local>
2017-01-27 17:13:19 +0100

    minor fixes

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ var google = require('google');
 var extractor = require('unfluff');
 var cheerio = require('cheerio');
 var request = require('request');
+var util = require('util');
+var twit = require('twitter');
+var client = new twit({
+    consumer_key: 'd5ZLgfJJPVCpLHRRlQfWz5VR5',
+    consumer_secret: 'AZng4nqOX2gt8IG2b4BeNoqwfyLmiQIlCQnNKuS6OwgWRCHbDq',
+    access_token_key: '741713163574509568-PalYvOA2P7hY7YovXCZMNKtJQZHuVD6',
+    access_token_secret: 'OrBPIOfGtquS8ryEAB9AxkdY1dpOmScJkKBL4yDSS2VJZ'
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -31,17 +39,16 @@ app.use(bodyParser.json());
 app.use(cookieParser());
  app.use(express.static(path.join(__dirname, 'public')));
 
-
 /** Public Variables **/
 
 var sources = [];
 var list = [];
+var result = [];
 var html;
 
 
 
 /** Sorting Methods **/
-
 
 var sourceString = function(){
   var string = 'site: ';
@@ -64,41 +71,50 @@ function getTrueKeys(obj) {
 }
 
 
-
-
 /* HTTP requests */
 
+/* Search on Google */
 
 app.post('/search', function (req, res) {
-
-
+    
   sources= getTrueKeys(req.body.sourceArray);
   var searchQuery = req.body.searchQuery;
 
   google.resultsPerPage = 25
-   var nextCounter = 0
 
     console.log(sourceString() + ' ' + searchQuery);
      google(sourceString() + ' ' + searchQuery, function (err, response){
     if (err) console.error(err)
 
-
          list = response.links;
-
-
-         /*if (nextCounter < 4) {
-           nextCounter += 1
-           if (res.next) res.next()
-         }*/
          res.json(list);
   })
 
 
-
-
-
 })
 
+/** Search on Twitter **/
+
+app.post('/searchTwitter', function (req, res) {
+
+    var searchQuery = req.body.searchQuery;
+
+    if(sources.indexOf('https://twitter.com/') >= 0){
+        
+        var params = {q: searchQuery, lang:'en', resultType: 'mixed', exclude:'retweets', count: 20};
+            client.get('search/tweets', params, function(error, data, response) {
+                if (!error) {
+                    var tweets = data.statuses;
+                    console.log(tweets[0].id_string);
+                    res.json(tweets);
+                }
+        
+        
+            });
+        }
+})
+
+/* Save to Database */
 
 app.post('/api', function (req, res) {
     var dbData = [];
@@ -124,29 +140,102 @@ app.post('/api', function (req, res) {
 
 })
 
+/* Get content from site */
+
 app.post('/unfluff', function (req, res) {
     var url =  req.body.unfluffUrl;
-     var dataContent=[];
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(body);
-            var html = $.html();
-            data = extractor(html, 'en');
-            var cleanText =  data.text.replace(/[\r\n]/g, '');
-            dataContent.push({
-                "title" : data.title,
-                "date": data.date,
-                "tags": data.tags,
-                "description": data.description,
-                "publisher" : data.publisher,
-                "text": cleanText
-            });
-            res.json(dataContent);
+    var dataContent = [];
 
-        } else {
-            console.log("We’ve encountered an error: " + error);
-        }
-    });
+    /* Get CVE information */
+
+    if (url.toLowerCase().indexOf("web.nvd.nist.gov/view/vuln/") >= 0){
+
+        request(url, function (error, response, body) {
+
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body);
+
+                /*Get CVE title */
+
+                var title = $('div.vuln-detail').find('h3').text();
+
+                /*Get CVE summary*/
+
+                var summary =  $('#BodyPlaceHolder_cplPageContent_plcZones_lt_zoneCenter_VulnerabilityDetail_VulnFormView').find('h3').text();
+
+                /*Get Cve Date*/
+
+                var cveDate =  $('#BodyPlaceHolder_cplPageContent_plcZones_lt_zoneCenter_VulnerabilityDetail_VulnFormView').find('div.row').first().text();
+                cveDate = cveDate.replace(/[\r\n]/g, '');
+                cveDate = cveDate.replace(/  +/g, ' ');
+                cveDate = cveDate.trim();
+                if(!cveDate) {
+                    cveDate = "No info";
+                }
+                /*Get CVSS20 Score */
+
+                var cvss20 =  $('#BodyPlaceHolder_cplPageContent_plcZones_lt_zoneCenter_VulnerabilityDetail_VulnFormView_Vuln2CvssPanel').find('div.row').first().text();
+                cvss20 = cvss20.replace(/[\r\n]/g, '');
+                cvss20 = cvss20.replace(/  +/g, ' ');
+                cvss20 = cvss20.trim();
+                if(!cvss20) {
+                    cvss20 = "No info";
+                }
+                /* overview*/
+                var overview = $('div.vuln-detail').find('h4:contains("Overview")').next().text();
+                if(!overview){
+                }
+                /*Get CVSS30 Score */
+
+                var cvss30 =  $('#BodyPlaceHolder_cplPageContent_plcZones_lt_zoneCenter_VulnerabilityDetail_VulnFormView_Vuln3CvssPanel').find('div.row').first().text();
+                cvss30 = cvss30.replace(/[\r\n]/g, '');
+                cvss30 = cvss30.replace(/  +/g, ' ');
+                cvss30 = cvss30.trim();
+                if(!cvss30) {
+                    cvss30 = "No info";
+                }
+                dataContent.push({
+                    "title"         :   title,
+                    "URL"           :   url,
+                    "Summary"       :   summary,
+                    "Overview"      :   overview,
+                    "cveDate"       :   cveDate,
+                    "cvss20"        :   cvss20,
+                    "cvss30"        :   cvss30
+                });
+                res.json(dataContent);
+            } else {
+                console.log("We’ve encountered an error: " + error);
+            }
+
+        });
+
+    }else {
+
+        /* Get content from site*/
+
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body);
+                var html = $.html();
+                data = extractor(html, 'en');
+                var cleanText = data.text.replace(/[\r\n]/g, '');
+                dataContent.push({
+                    "title": data.title,
+                    "date": data.date,
+                    "tags": data.tags,
+                    "description": data.description,
+                    "publisher": data.publisher,
+                    "text": cleanText
+                });
+                res.json(dataContent);
+
+            } else {
+                console.log("We’ve encountered an error: " + error);
+            }
+        });
+    }
+
 })
 
 module.exports = app;
--- public/Controllers/controller.js
+++ public/Controllers/controller.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', []);
 
 
-app.controller('UserController', function($scope, $http) {
+app.controller('UserController', function($scope, $http, $timeout) {
 
     $scope.sources = {};
     $scope.urlSource = {};
@@ -9,42 +9,67 @@ app.controller('UserController', function($scope, $http) {
     $scope.rating = {};
     $scope.typeOf = {};
 
+    var sourceList =[];
+
     $scope.searchBtn = true;
     $scope.oldModal = true;
 
+
+
+
     $scope.search = function() {
+
         console.log($scope.sources);
         console.log($scope.searchQuery);
 
         $scope.searchBtn = false;
         $scope.loading = true;
+        $scope.searchCol = false;
+        $scope.twitterCol = false;
 
-        $http.post('/search', {sourceArray: $scope.sources, searchQuery: $scope.searchQuery}).success(function(res){
+        //* Google Search *//
+
+        $http.post('/search', {sourceArray: $scope.sources, searchQuery: $scope.searchQuery}).success(function (res) {
 
             $scope.list = res;
             console.log($scope.list);
 
+
             $scope.searchBtn = true;
             $scope.loading = false;
+            $scope.searchCol = true;
         });
 
+        //* Twitter Search *//
 
-        
+        if($scope.sources.hasOwnProperty('https://twitter.com/') && $scope.sources['https://twitter.com/'] === true)
+        {
+            $http.post('/searchTwitter', {searchQuery: $scope.searchQuery}).success(function (res) {
 
-    };
+                $scope.twitterList = res;
+                $scope.twitterCol = true;
+                console.log($scope.twitterList);
+            });
+        }
 
+
+    }
+
+    //* Add to database call *//
 
     $scope.addToDb = function(theUrl, index, theDate, theTags) {
-
         $http.post('/api', {url: theUrl, com: $scope.comment[index], rate:$scope.rating[index], date: theDate, tags: theTags, type: $scope.typeOf[index]}).success(function(res){
-
         });
-
+        $scope.addedToDb = true;
+        $timeout(function () { $scope.addedToDb = false; }, 4000);
     };
+
+    //* Get article information call *//
 
     $scope.unfluff = function(uUrl) {
         $scope.loadingModal = true;
         $scope.oldModal = false;
+
 
 
         $http.post('/unfluff', {unfluffUrl: uUrl}).success(function(res){
